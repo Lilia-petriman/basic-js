34468560119b99521de582d6e5d6793165bbef9f
Lilia Petriman <22Lilia0168@mail.ru>
2025-01-06 02:33:38 +0300

    feat: add 1 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity ) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+
+  if (
+    typeof sampleActivity !== "string" ||
+    isNaN(+sampleActivity) ||
+    arguments.length === 0 ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+  )
+    return false;
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  );
   // remove line with error and write your code here
 }
 
